d4d8a9a9619fe038b000aea33e1ffb58518327dc
Joanna092 <kochanowska92@gmail.com>
2024-03-25 08:10:11 +0900

    added title to the note

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+ //Create a child h1 element for the sticky note header NEW
+ //var newH1 = document.createElement('h1');
+ //var newTextNode = document.createTextNode('Hello World!');
+ //newH1.appendChild(newTextNode);
+ //stickyHeader.appendChild(newH1)
+
+
+
 document.addEventListener('DOMContentLoaded', function() {
   // Code placed here will run when the DOM content has loaded.
   var noteList = document.getElementById('note-list');
+  var noteTitle = document.getElementById('note-title');
   var noteInput = document.getElementById('note-input');
   var addButton = document.getElementById('add-button');
 
@@ -14,8 +23,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
  // Create a div element for the sticky note header.
  var stickyHeader = document.createElement('div');
- // Give it a class of sticky-header.
- stickyHeader.setAttribute('class', 'sticky-header');
 
  // Create a span element for the close button.
  var closeButton = document.createElement('span');
@@ -32,6 +39,12 @@ document.addEventListener('DOMContentLoaded', function() {
  // Append the close button to the sticky header.
  stickyHeader.appendChild(closeButton);
 
+ // Give it a class of sticky-header.
+ stickyHeader.setAttribute('class', 'sticky-header');
+
+ var stickyTitle = document.createElement('h1');
+stickyTitle.innerHTML = 'Hello World!'
+
 //create a div for the content of the note
  var stickyContent = document.createElement('div');
 
@@ -40,21 +53,21 @@ document.addEventListener('DOMContentLoaded', function() {
  // Make the content non-editable.
  stickyContent.setAttribute('contenteditable', 'false');
  // Catch content from the input field 
- stickyContent.innerHTML = noteInput.value;
+ stickyContent.innerHTML = noteTitle.value;
 
 //Add noteText to the div
  newStickyNote.appendChild(stickyHeader);
+ newStickyNote.appendChild(stickyTitle)
  newStickyNote.appendChild(stickyContent);
 
 // Append noteList to the note-list div
   noteList.appendChild(newStickyNote );
- 
 };
 
 addButton.addEventListener('click', function () {
     addNote();
     // Clear the input element by setting it to empty string.
-    noteInput.value = '';
+    noteTitle.value = '';
   });
 });
 
