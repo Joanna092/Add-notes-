55142827bb5f04119b9119e40e262f1313d2b61f
Joanna092 <kochanowska92@gmail.com>
2024-04-14 17:58:38 +0900

    clear the photo after adding a note

--- index.js
+++ index.js
@@ -114,6 +114,7 @@ document.addEventListener("DOMContentLoaded", function () {
       addNote();
       noteTitle.value = "";
       contentInput.value = "";
+      photoInput.value = []
     }
   });
 
